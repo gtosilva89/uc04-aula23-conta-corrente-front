a237d463ea23d7407470ce79dba61bda510668d7
Gustavo Silveira Silva <silva.gustavo@hotmail.com.br>
2025-02-14 19:46:39 -0300

    14-02 atualizações professor

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
-
-
 import { useForm } from '@mantine/form'
 import './App.css'
 import { Button, PasswordInput, TextInput } from '@mantine/core'
-import { useCallback } from 'react'
+import { useCallback, useState } from 'react'
 
 type formValues = {
   cpf: string;
@@ -19,9 +17,38 @@ function App() {
     }
   })
 
-  const handleForm = useCallback(({ cpf, password }: formValues) => {
+  const [status, setStatus] = useState('Não Logado');
+
+  const handleForm = useCallback(async ({ cpf, password }: formValues) => {
     console.log(cpf, password)
-  }, [])
+    const loginData = JSON.stringify({
+      cpf,
+      password
+    })
+    console.log(loginData)
+
+    const options = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: `{"cpf":"${cpf}","password":"${password}"}`
+    };
+    try {
+      const response = await fetch("http://172.17.9.86:3000/contas/auth", options)
+      const responseBody = await response.json();
+      const { token } = responseBody
+      if (token) {
+        console.log(token)
+        setStatus("Logado")
+      }
+
+    } catch (error) {
+      console.log(error);
+    }
+
+
+  }, []);
+
+
   return (
     <>
       <form onSubmit={form.onSubmit(handleForm)}>
@@ -42,6 +69,7 @@ function App() {
           Submit
         </Button>
       </form>
+      <h1>{status}</h1>
     </>
   )
 }
